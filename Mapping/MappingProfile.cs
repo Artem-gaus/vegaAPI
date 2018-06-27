d8aa3b20a4f732d1ef4dcaa58ad60309764a458d
Artem-gaus <gausin.artem@gmail.com>
2018-06-27 23:47:13 +0300

    Mapping API resource to Domain model

--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using AutoMapper;
 using vegaAPI.Controllers.Resources;
 using vegaAPI.Models;
@@ -8,9 +9,17 @@ namespace vegaAPI.Mapping
     {
         public MappingProfile()
         {
+            //Domain to API Resource
             CreateMap<Make, MakeResource>();
             CreateMap<Model, ModelResources>();
             CreateMap<Feature, FeatureResource>();
+
+            //API Resource to Domain
+            CreateMap<VehicleResource, Vehicle>()
+                .ForMember(v => v.ContactName, opt => opt.MapFrom(vr => vr.Contact.Name))
+                .ForMember(v => v.ContactPhone, opt => opt.MapFrom(vr => vr.Contact.Phone))
+                .ForMember(v => v.ContactEmail, opt => opt.MapFrom(vr => vr.Contact.Email))
+                .ForMember(v => v.Features, opt => opt.MapFrom(vr => vr.Features.Select(id => new VehicleFeature { FeatureId = id })));
         }
     }
 }
